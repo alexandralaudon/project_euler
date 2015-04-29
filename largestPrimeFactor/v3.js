cd4f598be160388bec71e88ce7fb58df039c82f4
Alex <alexandralaudon@gmail.com>
2015-04-29 14:54:48 -0600

    organized functions

--- largestPrimeFactor/v3.js
+++ largestPrimeFactor/v3.js
@@ -1,10 +1,3 @@
-var aPrime = function(testForPrime){
-  for (var x = 2; x <= testForPrime/2 ; x++){
-    if (testForPrime%x === 0) return false;
-  }
-  return true;
-};
-
 var largestPrimeFactor = function(test){
   var primeFactor = 1;
 
@@ -16,6 +9,13 @@ var largestPrimeFactor = function(test){
   }
 
   return primeFactor;
+};
+
+var aPrime = function(testForPrime){
+  for (var x = 2; x <= testForPrime/2 ; x++){
+    if (testForPrime%x === 0) return false;
+  }
+  return true;
 };
 
 console.log(largestPrimeFactor(600851475143));
