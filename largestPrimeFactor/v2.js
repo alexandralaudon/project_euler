eeea3105aa22396cd7ee3e661ebf443d3ea484a4
Alex <alexandralaudon@gmail.com>
2015-04-29 14:34:01 -0600

    Completed largest prime factor

---
+++ largestPrimeFactor/v2.js
@@ -0,0 +1,37 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143?
+
+// Example: 13195
+
+// If 13195%2 === 0, find the element that makes up 13195 using 13195/2.
+//    then check if that is a prime number.
+// If 13195%3 === 0, find the element that makes up 13195 using 13195/3.
+//    then check if that is a prime number
+
+
+var aPrime = function(testForPrime){
+  for (var x = 2; x <= testForPrime/2 ; x++){
+    if (testForPrime%x === 0) return false;
+  }
+  return true;
+};
+
+var largestPrimeFactor = function(test){
+  var endPoint = Math.round(test/2),
+      primeFactor = 0,
+      oppositeNumber = 0;
+
+  for (var i = 2; i <= endPoint ; i++){
+    if (test%i === 0){
+      oppositeNumber = test/i;
+      if (aPrime(oppositeNumber)){
+        primeFactor = oppositeNumber;
+        break;
+      }
+    }
+  }
+
+  return primeFactor;
+};
+
+console.log(largestPrimeFactor(600851475143));
