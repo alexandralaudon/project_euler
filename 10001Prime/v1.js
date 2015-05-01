3789ba532d4b1051ad7f163d1d6f8bc977d49b81
Alex <alexandralaudon@gmail.com>
2015-04-30 18:55:28 -0600

    completed 10001 Prime

---
+++ 10001Prime/v1.js
@@ -0,0 +1,31 @@
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+//we can see that the 6th prime is 13.
+
+//What is the 10 001st prime number?
+
+// calculate what is a prime number?
+// enter a loop counting up to the maximum number, how many prime numbers there are.
+
+var isPrime = function(number){
+  for (var i = 2; i < number/2; i++){
+    if (number%i === 0) return false;
+  }
+  return true;
+};
+
+var countPrime = function(maximum){
+  var primeNumber = 0,
+      count = 0,
+      i = 2;
+
+  while(count <= maximum){
+    if (isPrime(i)) {
+      primeNumber = i;
+      count++;
+    }
+    i++;
+  }
+  return primeNumber;
+};
+
+console.log(countPrime(10001));
